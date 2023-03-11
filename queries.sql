f1606fa4fef1d4519ae70056aad127c454168374
mirnamp <ampueromirna8@gmail.com>
2023-03-11 18:54:08 -0500

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -14,6 +14,31 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = true;
 
-SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE name <> 'Gabumon';SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escaped = false;
+
+SELECT AVG(weight) FROM animals;
+
+SELECT neutered, COUNT(*) FROM animals WHERE escaped = true GROUP BY neutered;
+
+SELECT species, MIN(weight), MAX(weight) FROM animals GROUP BY species;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight) FROM animals;
+SELECT neutered, SUM(escape_attempts) as total_escapes
+FROM animals
+GROUP BY neutered
+ORDER BY total_escapes DESC;
+SELECT species, MIN(weight) AS min_weight, MAX(weight) AS max_weight
+FROM animals
+GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE birthdate BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
+
